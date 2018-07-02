7de4eeb3cfd1e9524a2163fcfefb087788cea0da
Simon Bull <simon.bull.84@googlemail.com>
2018-07-02 14:52:52 +0100

    Add services route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const config = require('./infrastructure/config');
 const healthCheck = require('login.dfe.healthcheck');
+const services = require('./app/services');
 
 const mountRoutes = (app) => {
   app.use('/healthcheck', healthCheck({ config }));
+
+  app.use('/services', services());
 };
 
 module.exports = mountRoutes;
