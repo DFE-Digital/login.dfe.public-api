9e33322b1bc0d56e0584e63a72e3a234a7ea9e67
Simon Bull <simon.bull.84@googlemail.com>
2018-07-03 08:23:01 +0100

    Add correlation ids

--- src/routes.js
+++ src/routes.js
@@ -9,16 +9,24 @@ const mountRoutes = (app) => {
   app.use('/services', services());
 
   app.use((err, req, res, next) => {
-    logger.error(`Unhandled error processing ${req.url} - ${err.message}`);
+    logger.error(`Unhandled error processing ${req.url} - ${err.message} [server id: ${req.correlationId}, client id: ${req.clientCorrelationId}]`, {
+      url: req.url,
+      correlationId: req.correlationId,
+      clientCorrelationId: req.clientCorrelationId,
+    });
+
     if (res.headersSent) {
       return next(err);
     }
 
-    const body = config.hostingEnvironment.env === 'dev' ? {
-      code: err.code,
-      message: err.message,
-      stack: err.stack
-    } : undefined;
+    const body = {
+      id: req.correlationId,
+    };
+    if (config.hostingEnvironment.env === 'dev') {
+      body.code = err.code;
+      body.message = err.message;
+      body.stack = err.stack;
+    }
     res.status(500).send(body);
   });
 };
