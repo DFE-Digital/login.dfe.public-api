26433d845a126a8924696032377e9bc156147071
Simon Bull <simon.bull.84@googlemail.com>
2018-07-12 15:56:36 +0100

    Get clients from hot config api

--- src/infrastructure/hotConfig/api.js
+++ src/infrastructure/hotConfig/api.js
@@ -1,10 +1,31 @@
+const config = require('./../config');
+const KeepAliveAgent = require('agentkeepalive').HttpsAgent;
+const rp = require('request-promise').defaults({
+  agent: new KeepAliveAgent({
+    maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+    maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+    timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+    keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
+  }),
+});
+const jwtStrategy = require('login.dfe.jwt-strategies');
+
 const list = async () => {
-  return Promise.resolve([]);
+  const token = await jwtStrategy(config.hotConfig.service).getBearerToken();
+
+  return rp({
+    method: 'GET',
+    uri: `${config.hotConfig.service.url}/oidcclients`,
+    headers: {
+      authorization: `bearer ${token}`,
+    },
+    json: true,
+  });
 };
 
 const getClientByServiceId = async (serviceId) => {
   const all = await list();
-  return all.filter(x => x.params && x.params.serviceId && x.params.serviceId.toLowerCase() === serviceId.toLowerCase());
+  return all.find(x => x.params && x.params.serviceId && x.params.serviceId.toLowerCase() === serviceId.toLowerCase());
 };
 
 module.exports = {
