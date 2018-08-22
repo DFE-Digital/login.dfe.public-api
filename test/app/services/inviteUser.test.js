e701be834b520830ac552c2a3ee770643d77c054
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-22 10:49:21 +0100

    Updated tests

--- test/app/services/inviteUser.test.js
+++ test/app/services/inviteUser.test.js
@@ -22,18 +22,20 @@ describe('When inviting a user', () => {
 
   beforeEach(() => {
     getClientByServiceId.mockReset().mockReturnValue({
-      friendlyName: 'DfE Sign-in',
-      client_id: 'test',
-      client_secret: 'test',
-      redirect_uris: [
-        'https://localhost:41011/auth/cb'
-      ],
-      post_logout_redirect_uris: [
-        'https://localhost:41011/signout/complete',
-      ],
-      params: {
-        serviceId: 'da03ea7a-6c5b-4864-be53-2eaccf63bec4'
-      }
+      relyingParty: {
+        friendlyName: 'DfE Sign-in',
+        client_id: 'test',
+        client_secret: 'test',
+        redirect_uris: [
+          'https://localhost:41011/auth/cb'
+        ],
+        post_logout_redirect_uris: [
+          'https://localhost:41011/signout/complete',
+        ],
+      },
+        params: {
+          serviceId: 'da03ea7a-6c5b-4864-be53-2eaccf63bec4'
+        }
     });
 
     sendInvitationRequest.mockReset();
