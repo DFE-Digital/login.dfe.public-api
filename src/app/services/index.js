2004bc65ab7424a3888fbfb0e47fc472de14bdc9
Floyd Price <f.price@floydprice.com>
2019-01-23 14:34:15 +0000

    Revert "Nsa 1855 generate secret"

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -3,7 +3,6 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const inviteUser = require('./inviteUser');
 const getUsersAccess = require('./getUsersAccess');
-const getNewSecret = require('./getNewSecret');
 
 const area = () => {
   const router = express.Router();
@@ -11,8 +10,7 @@ const area = () => {
   router.post('/:sid/invitations', asyncWrapper(inviteUser));
   router.get('/:sid/organisations/:oid/users/:uid', asyncWrapper(getUsersAccess));
   // router.get('/:sid/users/:uid', asyncWrapper(getUsersAccess)); // TODO: Allow this once users can be mapped without organisation
-  router.get('/generate-secret', asyncWrapper(getNewSecret));
-  
+
   return router;
 };
 
