24ddfad285362d7b007c0ef8147ad8b9d01ecbe1
Simon Bull <simon.bull.84@googlemail.com>
2019-01-31 20:43:50 +0000

    Unpublish announcement

--- src/app/organisations/index.js
+++ src/app/organisations/index.js
@@ -3,6 +3,7 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const getOrganisatiobByTypeAndIdentifier = require('./getOrganisatiobByTypeAndIdentifier');
 const upsertAnnouncement = require('./upsertAnnouncement');
+const deleteAnnouncement = require('./deleteAnnouncement');
 
 
 const area = () => {
@@ -11,6 +12,7 @@ const area = () => {
   router.get('/find-by-type/:type/:identifier', asyncWrapper(getOrganisatiobByTypeAndIdentifier));
 
   router.post('/announcements', asyncWrapper(upsertAnnouncement));
+  router.delete('/announcements/:messageId', asyncWrapper(deleteAnnouncement));
 
   return router;
 };
