e89e0f15f0f1b3007f5b0245995c3fbefa8984f0
Simon Bull <simon.bull.84@googlemail.com>
2019-02-19 13:45:50 +0000

    Create sub-service

---
+++ src/app/services/createService.js
@@ -0,0 +1,57 @@
+const uuid = require('uuid/v4');
+const { generatePassphrase } = require('niceware');
+const { createService: createServiceInApi } = require('./../../infrastructure/applications');
+
+const parseAndValidateModel = (req) => {
+  const model = {
+    service: {
+      name: req.body.name,
+      description: req.body.description,
+      redirectUris: req.body.redirectUris || [],
+    },
+    validationErrors: [],
+  };
+
+  if (!model.service.name) {
+    model.validationErrors.push('Must provide name');
+  }
+
+  if (model.service.redirectUris.length < 1) {
+    model.validationErrors.push('Must provide at least 1 redirectUris');
+  }
+
+  return model;
+};
+const createService = async (req, res) => {
+  const model = parseAndValidateModel(req);
+  if (model.validationErrors.length > 0) {
+    return res.status(400).json({ reasons: model.validationErrors });
+  }
+
+  const service = {
+    name: model.service.name,
+    description: model.service.description,
+    isExternalService: false,
+    parentId: req.client.id,
+    relyingParty: {
+      client_id: `c-${uuid()}`,
+      client_secret: generatePassphrase(8).join('-'),
+      redirect_uris: model.service.redirectUris,
+      grant_types: ['authorization_code', 'refresh_token'],
+      params: {
+        hideApprover: 'true',
+        hideSupport: 'true',
+        explicitConsent: 'true',
+      },
+    },
+  };
+  const createdService = await createServiceInApi(service);
+  return res.status(201).json({
+    name: createdService.name,
+    description: createdService.description,
+    clientId: createdService.relyingParty.client_id,
+    clientSecret: createdService.relyingParty.client_secret,
+    redirectUris: createdService.relyingParty.redirect_uris,
+  });
+};
+module.exports = createService;
