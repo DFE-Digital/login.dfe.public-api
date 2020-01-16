a4a287b9c78416551873e873b19eb1f73e1c1e5c
jovin-joy <jovin.joy@education.gov.uk>
2020-01-16 14:48:13 +0000

    NSA-2297 - changes for get users by roles

--- src/app/organisations/getUsersByRoles.js
+++ src/app/organisations/getUsersByRoles.js
@@ -5,7 +5,8 @@ const { usersByIds } = require('../../infrastructure/directories');
 
 const getUsersByRoles = async (req, res) => {
     const { correlationId, clientCorrelationId } = req;
-    const { roles } = req.query;
+    const roles = req.query.roles?JSON.parse(req.query.roles):null;
+
     try {
         logger.info(`Getting users for UKPRN ${req.params.id} (correlationId: ${correlationId}, client correlationId: ${clientCorrelationId}`, {
             correlationId,
@@ -21,24 +22,24 @@ const getUsersByRoles = async (req, res) => {
     }
     // Get all users associated with that service
     const serviceUsers = await getServiceUsers(req.client.id,organisation.id,correlationId);
-    let usersDetails;
+    let userIdNRoles, usersDetails;
     if(serviceUsers && serviceUsers.services){
-        const userIds = serviceUsers.services.filter(f=>f.roles.find(role => roles.includes(role.code))).map(user=>user.userId);
+        userIdNRoles = serviceUsers.services.filter(f=>f.roles.find(role => roles.includes(role.code))).
+        map((user)=> {return {id: user.userId,roles: user.roles.map(role=>role.code)}});
         // Get user details by user ids
-        if(userIds) {
+        if(userIdNRoles && userIdNRoles.length) {
+            const userIds = userIdNRoles.map(ids=>ids.id);
             usersDetails = await usersByIds(userIds.join(','), req.correlationId);
+            const users = usersDetails.map((user)=>{
+                const role = userIdNRoles.find((ids)=>ids.id===user.sub);
+                return {email:user.email,firstName: user.given_name, lastName: user.family_name, roles: role? role.roles: null };
+            });
+            return res.json(
+                {ukprn:req.params.id, users}
+            );
+        }else{
+            return res.status(404).send();
         }
-    }else{
-        return res.status(404).send();
-    }
-
-    if(usersDetails) {
-        const users = usersDetails.map((user)=>{
-            return {email:user.email,firstName: user.given_name, lastName: user.family_name, roles};
-        });
-        return res.json(
-            {ukprn:req.params.id, users}
-        );
     }else{
         return res.status(404).send();
     }
