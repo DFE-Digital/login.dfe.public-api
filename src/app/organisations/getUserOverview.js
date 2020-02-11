d276e30dc3e4d4968261001c4a65af9cd7432f92
jovin-joy <jovin.joy@education.gov.uk>
2020-02-11 09:18:37 +0000

    NSA-2729

--- src/app/organisations/getUserOverview.js
+++ src/app/organisations/getUserOverview.js
@@ -7,7 +7,6 @@ const getUserOverview = async (req, res) => {
     const { correlationId, clientCorrelationId } = req;
     const { roles, page, pageSize } = req.query;
     const ukprn = req.params.id;
-
     try {
         logger.info(`Getting users for UKPRN ${req.params.id} (correlationId: ${correlationId}, client correlationId: ${clientCorrelationId}`, {
             correlationId,
@@ -22,52 +21,21 @@ const getUserOverview = async (req, res) => {
         return res.status(404).send();
     }
     // Get Roles by RoleCode
+    let serviceUsers = null;
     if(roles) {
         const allRoles = await getRoles(req.client.id, correlationId);
         const serviceRoleIds = allRoles.filter(role=> roles.includes(role.code)).map(m=>m.id);
-        const serviceUsers = await getServiceUsersV2(req.client.id,organisation.id, serviceRoleIds, page, pageSize, correlationId);
-        const result = serviceUsers.services.map((user) => {
-            return {id: user.userId, roles: user.roles}
-        });
-        return res.json(
-            {ukprn:req.params.id, result}
-        );
-
+        serviceUsers = await getServiceUsersV2(req.client.id,organisation.id, serviceRoleIds, page, pageSize, correlationId);
     }else{
-        const serviceUsers = await getServiceUsers(req.client.id,organisation.id,correlationId);
-        return serviceUsers.services.map((user) => {
-            return {id: user.userId, roles: user.roles.map(role => role.code)}
-        });
+        serviceUsers = await getServiceUsers(req.client.id,organisation.id,correlationId);
     }
-
-    // Get all users associated with that service
-    const serviceUsers = await getServiceUsers(req.client.id,organisation.id,correlationId);
-    let userIdNRoles, usersDetails;
-    if(serviceUsers && serviceUsers.services){
-        if(roles) {
-
-            userIdNRoles = serviceUsers.services.filter(f => f.roles.find(role => roles.includes(role.code))).map((user) => {
-                return {id: user.userId, roles: user.roles.map(role => role.code)}
-            });
-        }else{
-            userIdNRoles = serviceUsers.services.map((user) => {
-                return {id: user.userId, roles: user.roles.map(role => role.code)}
-            });
-        }
-        // Get user details by user ids
-        if(userIdNRoles && userIdNRoles.length) {
-            const userIds = userIdNRoles.map(ids=>ids.id);
-            usersDetails = await usersByIds(userIds.join(','), req.correlationId);
-            const result = usersDetails.map((user)=>{
-                const role = userIdNRoles.find((ids)=>ids.id===user.sub);
-                return {emails:[user.email],'name.givenname': [user.given_name], 'name.familyname': [user.family_name], Title:[''] };
-            });
-            return res.json(
-                {ukprn:req.params.id, result}
-            );
-        }else{
-            return res.status(404).send();
-        }
+    if(serviceUsers)
+    {
+        const userIds = serviceUsers.services.map(ids=>ids.userId);
+        const result = await getUserDetails(req,userIds.join(','));
+        return res.json(
+            {page: serviceUsers.page, totalPages: serviceUsers.totalNumberOfPages, totalRecords: serviceUsers.totalNumberOfRecords , result}
+        );
     }else{
         return res.status(404).send();
     }
@@ -80,4 +48,13 @@ const getUserOverview = async (req, res) => {
         throw e;
     }
 };
+
+const getUserDetails = async (req,userIds) =>
+{
+    const usersDetails = await usersByIds(userIds, req.correlationId);
+    return  usersDetails.map((user)=>{
+        return {emails:[user.email],'name.givenname': [user.given_name], 'name.familyname': [user.family_name], Title:[''] };
+    });
+};
+
 module.exports=getUserOverview;
