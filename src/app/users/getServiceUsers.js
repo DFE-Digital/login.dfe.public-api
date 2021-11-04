b5dd12d0e5ce462c56ab4bf9e4ff54f427270e25
Vijay <vijay.hirudayasamy@education.gov.uk>
2021-11-04 14:47:23 +0000

    NSA-5166 the logic to get the users belongs to the service has been upgraded to have filters. If there are filters then it follow new approach by getting the user first and get the user service otherwise it follows the current approach. New adapter configuration has been added for DAO pacakage.

--- src/app/users/getServiceUsers.js
+++ src/app/users/getServiceUsers.js
@@ -1,24 +1,60 @@
-const { extractPageParam, extractPageSizeParam } = require('../utils');
+const { extractPageParam, extractPageSizeParam, extractStatusParam, extractFromParam, extractToParam } = require('../utils');
 const { listServiceUsers } = require('../../infrastructure/organisations');
 const { usersByIds } = require('../../infrastructure/directories');
+const { directories } = require('login.dfe.dao');
 
 const listUsers = async (req, res) => {
   let page;
   let pageSize;
+  let status;
+  let to;
+  let from;
+  let fromDate;
+  let toDate;
+
   try {
     page = extractPageParam(req);
     pageSize = extractPageSizeParam(req);
+    status = extractStatusParam(req);
+    to = extractToParam(req);
+    from = extractFromParam(req);
+
+    if (status && status !== '1' && status !== '0') {
+      return res.status(400).send('status should be either 0 or 1');
+    }
+
+    if (to && isNaN(Date.parse(to))) {
+      return res.status(400).send('to date is not a valid date');
+    } else if (to) {
+      toDate = new Date(to);
+    }
+    if (from && isNaN(Date.parse(from))) {
+      return res.status(400).send('from date is not a valid date');
+    } else if (from) {
+      fromDate = new Date(from);
+    }
+
   } catch (e) {
     return res.status(400).send(e.message);
   }
 
-  const pageOfUserServices = await listServiceUsers(req.client.id, page, pageSize, req.correlationId);
-  const userIds = pageOfUserServices.users.map((user) => user.id);
-  const releventUsers = await usersByIds(userIds.join(','), req.correlationId);
+  let pageOfUserServices;
+  let users;
+  if (status || from || to) {
+    users = await directories.getUserWithFilters(status, fromDate, toDate, req.correlationId);
+    const userIds = users.map((user) => user.sub);
+    pageOfUserServices = await listServiceUsers(req.client.id, userIds, page, pageSize, req.correlationId);
+
+  } else {
+    pageOfUserServices = await listServiceUsers(req.client.id, null, page, pageSize, req.correlationId);
+    const userIds = pageOfUserServices.users.map((user) => user.id);
+    users = await usersByIds(userIds.join(','), req.correlationId);
+  }
+
   const mappedRecords = pageOfUserServices.users.map((userService) => {
-    const user = releventUsers.find((u)=> u.sub === userService.id);
+    const user = users.find((u) => u.sub === userService.id);
     let mappedUserService = {
-      approvedAt : userService.createdAt,
+      approvedAt: userService.createdAt,
       updatedAt: userService.updatedAt,
       organisation: userService.organisation,
       roleName: userService.role && userService.role.name ? userService.role.name : undefined,
@@ -28,16 +64,18 @@ const listUsers = async (req, res) => {
     if (user) {
       mappedUserService = Object.assign({
         ...mappedUserService,
-      }, {email: user.email, familyName: user.family_name, givenName: user.given_name});
+      }, { email: user.email, familyName: user.family_name, givenName: user.given_name });
     }
     return mappedUserService;
   });
-
   return res.send({
     users: mappedRecords,
     numberOfRecords: pageOfUserServices.totalNumberOfRecords,
     page: pageOfUserServices.page,
     numberOfPages: pageOfUserServices.totalNumberOfPages,
   });
+
+
 };
+
 module.exports = listUsers;
