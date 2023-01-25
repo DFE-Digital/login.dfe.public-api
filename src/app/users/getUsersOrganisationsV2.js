bdedc8da4b0c225f267fe1ce70849dabda6b61cc
Jovin Joy <57987577+jovin-joy@users.noreply.github.com>
2023-01-25 11:27:11 +0000

    feature/PIM-647 (#113)
    * Create .npmrc
    
    * 5166-More validation added
    with additional response message
    
    * 5166-type corrected
    
    * NSA-5533-
     login.dfe.audit.winston-sequelize-transport reference
     is updated in package.json
    
    * Implementation of changes required by DQT
    
    * Fix to broken test
    
    * v2 api
    
    * v2 api
    
    * v2 api
    
    * removing junk file
    
    * removing junk file
    
    Co-authored-by: Zeeshan Abbas <67509023+zeeshansGitHub@users.noreply.github.com>
    Co-authored-by: VijayMartinEdu <84141454+VijayMartinEdu@users.noreply.github.com>
    Co-authored-by: Vijay <vijay.hirudayasamy@education.gov.uk>
    Co-authored-by: Adam McCartney <adam.mccartney@education.gov.uk>
    Co-authored-by: Adam McCartney <94119319+AMcCartneyMTH@users.noreply.github.com>
    Co-authored-by: jo-joy <jovin.joy@outlook.com>

---
+++ src/app/users/getUsersOrganisationsV2.js
@@ -0,0 +1,29 @@
+const logger = require('./../../infrastructure/logger');
+const { getOrganisationsAssociatedWithUserV2 } = require('../../infrastructure/organisations');
+
+const getUserOrganisationsV2 = async (req, res) => {
+  const uid = req.params.id;
+  const { correlationId, clientCorrelationId } = req;
+  try {
+    logger.info(`Getting organisations for user ${uid} (correlationId: ${correlationId}, client correlationId: ${clientCorrelationId}`, {
+      correlationId,
+      clientCorrelationId
+    });
+
+    const userOrganisations = await getOrganisationsAssociatedWithUserV2(uid, correlationId);
+    if (!userOrganisations) {
+      return res.status(404).send();
+    }
+    const organisations = userOrganisations.map(x => x.organisation);
+    return res.send(organisations)
+  } catch (e) {
+    logger.info(`Error getting organisations for user ${uid} (correlationId ${correlationId}, client correlationId: ${clientCorrelationId}) - ${e.message}`, {
+      correlationId,
+      clientCorrelationId,
+      stack: e.stack,
+    });
+    throw e;
+  }
+};
+
+module.exports = getUserOrganisationsV2;
