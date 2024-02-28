cf31e17a360e1f651ef791d45017b9ae38c1ffc2
Adam McCartney <adam.mccartney@education.gov.uk>
2024-02-28 15:49:44 +0000

    UPDATED TO CORRELATIONID

--- src/app/services/serviceRoles.js
+++ src/app/services/serviceRoles.js
@@ -10,7 +10,7 @@ const getServiceRoles = async (req, res) => {
     clientCorrelationId
   });
 
-  const rolesForService = await getAllRolesForService(req.params.cid, req.correlationId);
+  const rolesForService = await getAllRolesForService(req.params.cid, req.params.correlationId);
   return res.send({
     roles: rolesForService.roles
   });
