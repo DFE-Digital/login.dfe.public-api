f95e410d69df5315a0e195b35f84dcaa7faee81b
Jared Marsh <jared.marsh@education.gov.uk>
2024-04-05 19:58:31 +0100

    Rewritten getServiceRoles tests to ensure they match expected functionality

--- test/app/services/getServiceRoles.test.js
+++ test/app/services/getServiceRoles.test.js
@@ -1,51 +1,78 @@
-jest.mock('./../../../src/infrastructure/config', () => require('../../utils').mockConfig());
-jest.mock('./../../../src/infrastructure/applications');
+const mockUtils = require('../../utils');
 
-const { mockResponse, mockRequest } = require('../../utils');
+const res = mockUtils.mockResponse();
+const mockLogger = mockUtils.mockLogger();
+
+jest.mock('../../../src/infrastructure/config', () => mockUtils.mockConfig());
+jest.mock('../../../src/infrastructure/logger', () => mockLogger);
+jest.mock('../../../src/infrastructure/applications');
+jest.mock('../../../src/infrastructure/access');
+
 const { getClientByServiceId } = require('../../../src/infrastructure/applications');
-const getService = require('../../../src/app/services/getService');
+const { getRoles } = require('../../../src/infrastructure/access');
+const getServiceRoles = require('../../../src/app/services/getServiceRoles');
 
-const res = mockResponse();
+const getRolesResult = [{
+  id: 'role-1',
+  name: 'Role One',
+  code: 'Role1',
+  numericId: '123456',
+  status: {
+    id: 1,
+  },
+}, {
+  id: 'role-2',
+  name: 'Role Two',
+  code: 'Role2',
+  numericId: '678910',
+  status: {
+    id: 1,
+  },
+}];
 
-describe('when getting specific service', () => {
+describe('When getting the roles for a service', () => {
   let req;
 
   beforeEach(() => {
-    req = mockRequest({
+    req = mockUtils.mockRequest({
       client: {
         id: 'parent-1',
         relyingParty: {
-          params: {
-            canCreateChildApplications: 'true',
-          },
+          client_id: 'req-clientId',
         },
       },
-    });
-    res.mockResetAll();
-
-    getAllRolesForService.mockReset().mockReturnValue({
-      id: 'service-1',
-      name: 'Service One',
-      description: 'First service',
-      parentId: 'parent-1',
-      relyingParty: {
-        client_id: 'csvc1',
-        client_secret: 'some-super-secure-secret',
-        redirect_uris: ['https://localhost:1234/auth/cb'],
+      params: {
+        clientid: 'svc-clientId',
       },
     });
+
+    res.mockResetAll();
+    mockLogger.mockResetAll();
+
+    getClientByServiceId.mockReset().mockReturnValue({
+      id: 'service-1',
+      parentId: 'parent-1',
+      relyingParty: {
+        client_id: 'svc-clientId',
+      },
+    });
+
+    getRoles.mockReset().mockReturnValue(getRolesResult);
   });
 
-  it('then it should return service role list', async () => {
+  it('then it should log the request', async () => {
     await getServiceRoles(req, res);
 
-    expect(res.send).toHaveBeenCalledTimes(1);
-    expect(res.send).toHaveBeenCalledWith({
-      clientId: 'csvc1'
+    expect(mockLogger.info).toHaveBeenCalledTimes(1);
+    expect(mockLogger.info).toHaveBeenCalledWith(`req-clientId is attempting to get service roles for: svc-clientId (correlationId: ${req.correlationId}, clientCorrelationId: ${req.clientCorrelationId})`, {
+      correlationId: req.correlationId,
+      clientCorrelationId: req.clientCorrelationId,
+      requester: 'req-clientId',
+      requestedClientId: 'svc-clientId',
     });
   });
 
-  it('then it should return not found if client does not exist', async () => {
+  it('then it should return 404 if the requested service does not exist', async () => {
     getClientByServiceId.mockReturnValue(undefined);
 
     await getServiceRoles(req, res);
@@ -55,13 +82,39 @@ describe('when getting specific service', () => {
     expect(res.send).toHaveBeenCalledTimes(1);
   });
 
-  it('then it should return not authorized if client not child of caller', async () => {
-    req.client.id = 'not-the-parent';
+  it('then it should return 403 if the requester is neither the parent of, or the requested service itself', async () => {
+    req.client.id = 'not-a-match';
+    req.client.relyingParty.client_id = 'not-a-match';
 
     await getServiceRoles(req, res);
 
     expect(res.status).toHaveBeenCalledTimes(1);
     expect(res.status).toHaveBeenCalledWith(403);
     expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('then it should return the roles if the requester is the parent of the requested service', async () => {
+    req.client.relyingParty.client_id = 'req-clientId';
+    await getServiceRoles(req, res);
+
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(getRolesResult);
+  });
+
+  it('then it should return the roles if the requester is the requested service', async () => {
+    req.client.relyingParty.client_id = 'svc-clientId';
+    await getServiceRoles(req, res);
+
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(getRolesResult);
+  });
+
+  it('then it should return an empty array if the requested service has no roles', async () => {
+    getRoles.mockReturnValue(undefined);
+
+    await getServiceRoles(req, res);
+
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith([]);
   });
 });
