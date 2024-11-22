23281ee5e9e198b051117edbd67055f8e8dece4e
Adam Mann <Adam.Mann@education.gov.uk>
2024-11-22 14:40:26 +0000

    Add comments to confusing code

--- src/app/organisations/getUsersByRoles.js
+++ src/app/organisations/getUsersByRoles.js
@@ -67,19 +67,27 @@ const getUsersByRoles = async (req, res) => {
       }
     }
     if (users.length) {
+      // This should be factored out into its own function for easier unit testing and comments
       const result = [];
+      // Loop over the users, remove duplicate records (by email) and combine the roles of these duplicate records
       users.forEach((user) => {
+        // existing equals all the users that have an email equal to the current user in the loop. Multiple records with the same
+        // email can result in existing having mutiple records
         const existing = users.filter((item) => user.email === item.email);
+        // This if statement can probably be removed.  It will never not have at least 1 value in it.
         if (existing.length) {
+          // existingIndex is -1 value if there is NOT another object matching existing[0] in results
           const existingIndex = result.indexOf(existing[0]);
           if (existingIndex !== -1) {
+            // result[1].roles becomes the deduped array of roles between result[1] and the currently searched user
             result[existingIndex].roles = [...new Set([...result[existingIndex].roles, ...user.roles])];
           } else {
+            // If nothing in existing matches an object in result, then push it because it must be unique
             result.push(existing[0]);
           }
         }
       });
-      ukprnOrUpin = isUPIN ? 'upin' : 'ukprn';
+      const ukprnOrUpin = isUPIN ? 'upin' : 'ukprn';
       return res.json(
         { [ukprnOrUpin]: req.params.id, users: result },
       );
