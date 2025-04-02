e27fb2316a96ebf38174ac0cd4d8a08861582323
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2025-04-02 10:27:53 +0100

    Docker Migration

--- src/infrastructure/config/index.js
+++ src/infrastructure/config/index.js
@@ -1,45 +1,142 @@
-
-
 const fs = require('fs');
-const Path = require('path');
+const os = require('os');
+const path = require('path');
 
-const getSettingsObject = (settings) => {
-  try {
-    return JSON.parse(settings);
-  } catch {
-    return null;
-  }
-};
+require('dotenv').config();
 
-const getSettingsFromFile = (settingsPath) => {
-  if (fs.existsSync(settingsPath)) {
-    const file = fs.readFileSync(settingsPath, 'utf8');
-    try {
-      return JSON.parse(file);
-    } catch {
-      return null;
+const config = {
+  loggerSettings: {
+    logLevel: "info",
+    applicationName: "Public-Api"
+  },
+  hostingEnvironment: {
+    env: process.env.LOCAL_ENV || "azure",
+    host: process.env.LOCAL_HOST || process.env.STANDALONE_PUBLIC_API_HOST_NAME,
+    port: process.env.LOCAL_PORT_PUBLIC-API || 443,
+    sslCert: process.env.LOCAL_SSL_CERT || "",
+    sslKey: process.env.LOCAL_SSL_KEY || "",
+    protocol: "https",
+    applicationInsights: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
+    agentKeepAlive: {
+      maxSockets: 50,
+      maxFreeSockets: 10,
+      timeout: 60000,
+      keepAliveTimeout: 30000
     }
-  }
-  return null;
-};
-
-const fetchConfig = () => {
-  if (process.env.settings) {
-    const settings = process.env.settings;
-    let settingsObject = getSettingsObject(settings);
-    if (settingsObject !== null) {
-      return settingsObject;
+  },
+  auth: {
+    type: "secret",
+    secret: process.env.PAPI_AUTH_SECRET
+  },
+  queue: {
+    connectionString: process.env.REDIS_CONN + "/4?tls=true"
+  },
+  notifications: {
+    connectionString: process.env.REDIS_CONN + "/4?tls=true"
+  },
+  adapter: {
+    type: "sequelize",
+    directories: {
+      host: process.env.PLATFORM_GLOBAL_SERVER_NAME,
+      username: "svcSigninDir",
+      password: process.env.SVC_SIGNIN_DIR_PASSWORD,
+      dialect: "mssql",
+      name: process.env.PLATFORM_GLOBAL_DIRECTORIES_DATABASE_NAME,
+      encrypt: true,
+      schema: "dbo",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    },
+    organisation: {
+      host: process.env.PLATFORM_GLOBAL_SERVER_NAME,
+      username: process.env.SVC_SIGNIN_ORG,
+      password: process.env.SVC_SIGNIN_ORG_PASSWORD,
+      dialect: "mssql",
+      name: process.env.PLATFORM_GLOBAL_ORGANISATIONS_DATABASE_NAME,
+      encrypt: true,
+      schema: "dbo",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
     }
-    const settingsPath = Path.resolve(settings);
-    if (fs.existsSync(settingsPath)) {
-      settingsObject = getSettingsFromFile(settingsPath);
-      if (settingsObject !== null) {
-        return settingsObject;
+  },
+  directories: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_DIRECTORIES_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANT_DOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  applications: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_APPLICATIONS_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANT_DOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  access: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_ACCESS_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANT_DOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
+      }
+    }
+  },
+  organisations: {
+    type: "api",
+    service: {
+      url: "https://" + process.env.STANDALONE_ORGANISATIONS_HOST_NAME,
+      auth: {
+        type: "aad",
+        tenant: process.env.PLATFORM_GLOBAL_TENANT_DOMAIN,
+        authorityHostUrl: process.env.TENANT_URL,
+        clientId: process.env.AAD_SHD_CLIENT_ID,
+        clientSecret: process.env.AAD_SHD_CLIENT_SECRET,
+        resource: process.env.AAD_SHD_APP_ID
       }
     }
   }
+}
 
-  throw new Error('Missing configuration');
-};
+// Persist configuration to a temporary file and then point the `settings` environment
+// variable to the path of the temporary file. The `login.dfe.dao` package can then load
+// this configuration.
+function mimicLegacySettings(config) {
+  // TODO: This can be improved by refactoring the `login.dfe.dao` package.
+  const tempDirectoryPath = fs.mkdtempSync(path.join(os.tmpdir(), 'config-'));
+  const tempConfigFilePath = path.join(tempDirectoryPath, 'config.json');
 
-module.exports = fetchConfig();
+  fs.writeFileSync(tempConfigFilePath, JSON.stringify(config), { encoding: 'utf8' });
+  process.env.settings = tempConfigFilePath;
+}
+
+mimicLegacySettings(config);
+
+module.exports = config; 
