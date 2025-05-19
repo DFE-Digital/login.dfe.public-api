46864a9f3522b2002f8eff481ef3c404eb8a2d8e
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-05-19 15:30:11 +0100

    Add package `login.dfe.api-client` and utilise its logger

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -1,91 +1,23 @@
-const { createLogger, format, transports, addColors } = require("winston");
-
-const { combine, prettyPrint, errors, simple, colorize } = format;
-const appInsights = require("applicationinsights");
-const AuditTransporter = require("login.dfe.audit.transporter");
-const AppInsightsTransport = require("login.dfe.winston-appinsights");
+const WinstonSequelizeTransport = require("login.dfe.audit.winston-sequelize-transport");
+const {
+  setupApplicationInsights,
+  setupLogging,
+} = require("login.dfe.api-client/logging");
 const config = require("../config");
 
-const logLevel =
-  config && config.loggerSettings && config.loggerSettings.logLevel
-    ? config.loggerSettings.logLevel
-    : "info";
+const additionalTransports = [];
 
-const customLevels = {
-  levels: {
-    audit: 0,
-    error: 1,
-    warn: 2,
-    info: 3,
-    verbose: 4,
-    debug: 5,
-    silly: 6,
-  },
-  colors: {
-    audit: "magenta",
-    error: "red",
-    warn: "yellow",
-    info: "blue",
-    verbose: "cyan",
-    debug: "green",
-    silly: "cyan",
-  },
-};
-
-addColors(customLevels.colors);
-
-// Formatter to hide audit records from other loggers.
-const hideAudit = format((info) =>
-  info.level.toLowerCase() === "audit" ? false : info,
-);
-
-const loggerConfig = {
-  levels: customLevels.levels,
-  transports: [],
-};
-
-loggerConfig.transports.push(
-  new transports.Console({
-    format: combine(hideAudit(), colorize({ all: true }), simple()),
-    level: logLevel,
-  }),
-);
-
-const opts = {
-  application: config.loggerSettings.applicationName,
-  level: "audit",
-};
-const auditTransport = AuditTransporter(opts);
-
-if (auditTransport) {
-  loggerConfig.transports.push(auditTransport);
+const sequelizeTransport = WinstonSequelizeTransport(config);
+if (sequelizeTransport) {
+  additionalTransports.push(sequelizeTransport);
 }
 
 if (config.hostingEnvironment.applicationInsights) {
-  appInsights
-    .setup(config.hostingEnvironment.applicationInsights)
-    .setAutoCollectConsole(false, false)
-    .setSendLiveMetrics(config.loggerSettings.aiSendLiveMetrics || false)
-    .start();
-  loggerConfig.transports.push(
-    new AppInsightsTransport({
-      format: combine(hideAudit(), format.json()),
-      client: appInsights.defaultClient,
-      applicationName: config.loggerSettings.applicationName || "PublicApi",
-      type: "event",
-      treatErrorsAsExceptions: true,
-    }),
-  );
+  setupApplicationInsights(config.hostingEnvironment.applicationInsights);
 }
 
-const logger = createLogger({
-  format: combine(simple(), errors({ stack: true }), prettyPrint()),
-  transports: loggerConfig.transports,
-  levels: loggerConfig.levels,
+module.exports = setupLogging({
+  applicationName: config.loggerSettings.applicationName || "PublicApi",
+  logLevel: config.loggerSettings?.logLevel,
+  additionalTransports,
 });
-
-process.on("unhandledRejection", (reason, p) => {
-  logger.error("Unhandled Rejection at:", p, "reason:", reason);
-});
-
-module.exports = logger;
