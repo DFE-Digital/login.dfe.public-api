73f2bc3b954537511c08a2ae5e4eacbf581cbfe6
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-05-29 12:43:56 +0100

    Replace `Applications.updateService` with `ApiClient.updateService`

--- src/infrastructure/applications/static.js
+++ src/infrastructure/applications/static.js
@@ -2,11 +2,6 @@ const destroyService = async () => {
   return Promise.resolve();
 };
 
-const updateService = async () => {
-  return Promise.resolve();
-};
-
 module.exports = {
   destroyService,
-  updateService,
 };
