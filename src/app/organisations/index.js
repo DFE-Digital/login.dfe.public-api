02ca9ff89771056d089001d3277d76fd009f47c1
Richard Ward <richard.ward@methods.co.uk>
2025-11-03 16:02:22 +0000

    Remove API `GET /organisations/user-overview/:id`
    Test Suites: 12 passed, 12 total
    Tests:       105 passed, 105 total
    Snapshots:   0 total
    Time:        1.382 s, estimated 2 s

--- src/app/organisations/index.js
+++ src/app/organisations/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const { asyncWrapper } = require("login.dfe.express-error-handling");
 const getUsersByRoles = require("./getUsersByRoles");
-const getUserOverview = require("./getUserOverview");
 
 const area = () => {
   const router = express.Router();
 
   router.get("/:id/users", asyncWrapper(getUsersByRoles));
-  router.get("/user-overview/:id", asyncWrapper(getUserOverview));
 
   return router;
 };
