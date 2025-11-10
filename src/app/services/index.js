c93308f45717ab48a70adaa6423903971797a10f
Richard Ward <richard.ward@methods.co.uk>
2025-11-10 10:21:07 +0000

    Remove API `GET /services/:clientid`

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const { asyncWrapper } = require("login.dfe.express-error-handling");
-
-const getService = require("./getService");
 const inviteUser = require("./inviteUser");
 const getUsersAccess = require("./getUsersAccess");
 const getServiceRoles = require("./getServiceRoles");
@@ -9,7 +7,6 @@ const getServiceRoles = require("./getServiceRoles");
 const area = () => {
   const router = express.Router();
 
-  router.get("/:clientid", asyncWrapper(getService));
   router.get("/:clientid/roles", asyncWrapper(getServiceRoles));
 
   router.post("/:sid/invitations", asyncWrapper(inviteUser));
@@ -17,7 +14,6 @@ const area = () => {
     "/:sid/organisations/:oid/users/:uid",
     asyncWrapper(getUsersAccess),
   );
-  // router.get('/:sid/users/:uid', asyncWrapper(getUsersAccess)); // TODO: Allow this once users can be mapped without organisation
 
   return router;
 };
